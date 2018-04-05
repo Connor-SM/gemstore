a8b0a329d709e1563c4d8ff746cff51b2b44c048
Connor-SM <cmilliken09@gmail.com>
2018-04-05 15:49:43 -0400

    Added cart functionality in the header and the side of the store page.

--- src/app/gem/gem.component.ts
+++ src/app/gem/gem.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit, Input } from '@angular/core';
 
 import { GemModel } from '../gem-model';
+import { CartModel } from '../cart-model';
 
 @Component({
   selector: 'app-gem',
@@ -10,6 +11,33 @@ import { GemModel } from '../gem-model';
 export class GemComponent implements OnInit {
 
   @Input() gem: GemModel;
+  @Input() cart: CartModel;
+
+  addToCart() {
+    this.gem.inventory -= 1;
+    this.cart.totalQuantity += 1;
+    this.cart.totalPrice += this.gem.price;
+
+    var flag = false;
+
+    for (let item of this.cart.items) {
+      if (item.gemid == this.gem.id) {
+        item.quantity += 1;
+        flag = true;
+      }
+    }
+
+    if (flag == false) {
+      this.cart.items.push(
+        {
+          gemid: this.gem.id
+          name: this.gem.name,
+          price: this.gem.price,
+          quantity: 1
+        }
+      );
+    };
+  }
 
   constructor() { }
 
--- src/app/navbar/navbar.component.ts
+++ src/app/navbar/navbar.component.ts
@@ -1,4 +1,5 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Input } from '@angular/core';
+import { CartModel } from '../cart-model';
 
 @Component({
   selector: 'app-navbar',
@@ -6,6 +7,9 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./navbar.component.css']
 })
 export class NavbarComponent implements OnInit {
+  title = "Gem Store"
+
+  @Input() cart: CartModel;
 
   constructor() { }
 
